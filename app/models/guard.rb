488eb54a5ed6374ae2fd11d8e73793947b3ed740
markbello <mcbello51286@gmail.com>
2018-01-19 17:57:44 -0500

    Ensure models and migrations working

--- app/models/guard.rb
+++ app/models/guard.rb
@@ -1,5 +1,5 @@
 class Guard < ApplicationRecord
   belongs_to :cell_block
   has_many :incident_reports
-  has_many :prisoners, through: incident_reports
+  has_many :prisoners, through: :incident_reports
 end
--- app/models/prisoner.rb
+++ app/models/prisoner.rb
@@ -1,5 +1,5 @@
 class Prisoner < ApplicationRecord
   belongs_to :cell
-  has_many :guards, through: incident_reports
+  has_many :guards, through: :incident_reports
   has_many :incident_reports
 end
