b4bb27ecaa0756b6e0965e647818873d268ae613
markbello <mcbello51286@gmail.com>
2018-01-22 16:45:29 -0500

    map out initial routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  resources :guard_prisoners
-  resources :incident_types
-  resources :incident_participants
+  #resources :guard_prisoners
+  #resources :incident_types
+  #resources :incident_participants
   resources :incident_reports
-  resources :cells
-  resources :cell_blocks
+  resources :cells, only: [:show]
+  resources :cell_blocks, only: [:index, :show]
   resources :prisoners
   resources :guards
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
