fd8e74a969f2ed2dc59e142d2acbb349a861ba25
markbello <mcbello51286@gmail.com>
2018-01-23 12:01:58 -0500

    Create login page

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,7 @@ Rails.application.routes.draw do
   resources :cell_blocks, only: [:index, :show]
   resources :prisoners
   resources :guards
+
+  get "login", to: "sessions#new", as: "login"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
