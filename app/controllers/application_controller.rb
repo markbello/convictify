9e3f34a90ff5cf93913e3870be8c11f363994e24
olegchursin <olegchursin@gmail.com>
2018-01-23 13:59:22 -0500

    Starting with auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,25 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :authorized
+  helper_method :logged_in?
+
+
+  def current_guard
+    if session[:guard_id]
+      @guard = Guard.find_by(id: session[:guard_id])
+    else
+    end
+  end
+
+
+  def logged_in?
+    !!current_guard
+  end
+
+
+  def authorized
+    redirect_to login_path unless logged_in?
+  end
+
 end
